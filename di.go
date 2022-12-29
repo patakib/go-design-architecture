fa87380f01fcdab1609268109f893fed764fa417
Balint Pataki <patakibalintka@gmail.com>
2022-12-29 20:55:53 +0100

    Create di.go

---
+++ di.go
@@ -0,0 +1,54 @@
+`
+DI is a pattern that allows a component to receive its dependencies (i.e., the objects it depends on) as constructor parameters, rather than creating them directly. This makes it easier to test the component in isolation and to swap out dependencies for different implementations.
+`
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+// Logger is an interface for logging messages.
+type Logger interface {
+	Log(message string)
+}
+
+// StdoutLogger is a concrete implementation of Logger that logs messages to stdout.
+type StdoutLogger struct{}
+
+// Log logs a message to stdout.
+func (l *StdoutLogger) Log(message string) {
+	fmt.Println(message)
+}
+
+// Service is a component that depends on a logger.
+type Service struct {
+	logger Logger
+}
+
+// NewService creates a new Service with the given logger.
+func NewService(logger Logger) *Service {
+	return &Service{logger: logger}
+}
+
+// DoSomething does something and logs a message.
+func (s *Service) DoSomething() {
+	s.logger.Log("Doing something...")
+}
+
+func main() {
+	logger := &StdoutLogger{}
+	service := NewService(logger)
+
+	if err := service.DoSomething(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+`
+In this example, the Logger interface and the StdoutLogger struct belong to the infrastructure layer, while the Service struct is part of the application's inner layer. The main function is the outer layer, and it depends on the inner layer (Service) to perform some action and log a message.
+
+The Service struct receives its Logger dependency as a constructor parameter, and this dependency is injected into the Service struct by the NewService function. This allows the `
+`
